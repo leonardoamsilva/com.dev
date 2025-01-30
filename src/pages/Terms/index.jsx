fa3bbd040d0ad29fb9594b3b32958c0f781765d2
Leonardo Anjos <leoanjosmachadosilva@gmail.com>
2025-01-30 18:39:42 -0300

    corrections

--- src/pages/Terms/index.jsx
+++ src/pages/Terms/index.jsx
@@ -8,105 +8,105 @@ export const Terms = () => {
         </header>
 
         <section>
-            <h2>Aceitação dos Termos</h2>
-            <p>Ao acessar ou usar o site <strong>comandos.dev</strong>, você concorda em
+            <h2>aceitação dos Termos</h2>
+            <p>ao acessar ou usar o site <strong>comandos.dev</strong>, você concorda em
                 cumprir e estar vinculado aos termos e condições descritos abaixo. Se você não concorda com estes
                 termos, por favor, não utilize o site.</p>
         </section>
 
         <section>
-            <h2>Descrição do Serviço</h2>
-            <p>O <strong>comandos.dev</strong> oferece uma plataforma onde desenvolvedores podem acessar, compartilhar
+            <h2>descrição do Serviço</h2>
+            <p>o <strong>comandos.dev</strong> oferece uma plataforma onde desenvolvedores podem acessar, compartilhar
                 e armazenar comandos úteis para várias plataformas e ferramentas utilizadas no desenvolvimento de
                 software. A plataforma oferece um repositório de comandos para facilitar o trabalho dos desenvolvedores.</p>
         </section>
 
         <section>
-            <h2>Uso do Serviço</h2>
-            <p>Você pode utilizar o <strong>comandos.dev</strong> para:</p>
+            <h2>uso do Serviço</h2>
+            <p>você pode utilizar o <strong>comandos.dev</strong> para:</p>
             <ul>
-                <li>Consultar e buscar comandos de várias plataformas.</li>
-                <li>Salvar, compartilhar e contribuir com novos comandos.</li>
+                <li>consultar e buscar comandos de várias plataformas.</li>
+                <li>salvar, compartilhar e contribuir com novos comandos.</li>
             </ul>
-            <p>Você concorda em utilizar o <strong>comandos.dev</strong> apenas para fins legais e de acordo com as leis
+            <p>você concorda em utilizar o <strong>comandos.dev</strong> apenas para fins legais e de acordo com as leis
                 aplicáveis à sua jurisdição.</p>
         </section>
 
         <section>
-            <h2>Conteúdo Gerado pelo Usuário</h2>
-            <p>Os usuários podem contribuir com comandos e outros conteúdos para o site. Ao enviar conteúdo, você
+            <h2>conteúdo Gerado pelo Usuário</h2>
+            <p>os usuários podem contribuir com comandos e outros conteúdos para o site. Ao enviar conteúdo, você
                 garante que tem o direito de compartilhá-lo e concorda com a seguinte licença:</p>
             <ul>
-                <li>Você concede ao <strong>comandos.dev</strong> uma licença não exclusiva, mundial, isenta de
+                <li>você concede ao <strong>comandos.dev</strong> uma licença não exclusiva, mundial, isenta de
                     royalties, transferível e sublicenciável para usar, modificar, adaptar, publicar e distribuir esse
                     conteúdo, de acordo com os fins do site.</li>
-                <li>Você é responsável pelo conteúdo que envia, incluindo sua conformidade com as leis de propriedade
+                <li>você é responsável pelo conteúdo que envia, incluindo sua conformidade com as leis de propriedade
                     intelectual e as normas de comportamento online.</li>
             </ul>
         </section>
 
         <section>
-            <h2>Comportamento e Proibições</h2>
-            <p>Ao utilizar o <strong>comandos.dev</strong>, você concorda em não:</p>
+            <h2>comportamento e Proibições</h2>
+            <p>ao utilizar o <strong>comandos.dev</strong>, você concorda em não:</p>
             <ul>
-                <li>Submeter ou compartilhar conteúdo que seja ilegal, ofensivo, prejudicial ou que viole os direitos de
+                <li>submeter ou compartilhar conteúdo que seja ilegal, ofensivo, prejudicial ou que viole os direitos de
                     propriedade intelectual de terceiros.</li>
-                <li>Realizar qualquer atividade que prejudique a integridade do site, como ataques cibernéticos,
+                <li>realizar qualquer atividade que prejudique a integridade do site, como ataques cibernéticos,
                     disseminação de malware, ou interferir no funcionamento do site.</li>
-                <li>Utilizar o site de forma a violar as leis ou regulamentos aplicáveis.</li>
+                <li>utilizar o site de forma a violar as leis ou regulamentos aplicáveis.</li>
             </ul>
         </section>
 
         <section>
-            <h2>Isenção de Responsabilidade</h2>
-            <p>Embora o <strong>comandos.dev</strong> se esforce para fornecer comandos úteis e precisos, o conteúdo
+            <h2>isenção de Responsabilidade</h2>
+            <p>embora o <strong>comandos.dev</strong> se esforce para fornecer comandos úteis e precisos, o conteúdo
                 compartilhado no site é fornecido "como está". O <strong>comandos.dev</strong> não garante a precisão,
                 confiabilidade ou eficácia dos comandos. O uso de qualquer comando compartilhado no site é de sua inteira
                 responsabilidade.</p>
-            <p>Não nos responsabilizamos por qualquer dano direto, indireto, incidental, especial ou consequencial
+            <p>não nos responsabilizamos por qualquer dano direto, indireto, incidental, especial ou consequencial
                 decorrente do uso do conteúdo fornecido no site, incluindo, mas não se limitando a, danos causados por
                 falhas ou erros nos comandos.</p>
         </section>
 
         <section>
-            <h2>Modificações no Serviço e Termos</h2>
-            <p>O <strong>comandos.dev</strong> reserva-se o direito de modificar, suspender ou descontinuar o site ou
+            <h2>modificações no Serviço e Termos</h2>
+            <p>o <strong>comandos.dev</strong> reserva-se o direito de modificar, suspender ou descontinuar o site ou
                 qualquer parte dele a qualquer momento, sem aviso prévio. Também podemos atualizar ou alterar estes
                 Termos de Uso a qualquer momento. Quaisquer alterações serão publicadas nesta página, e a data de revisão
                 será atualizada.</p>
-            <p>É de sua responsabilidade revisar regularmente os Termos de Uso para se manter informado sobre quaisquer
+            <p>é de sua responsabilidade revisar regularmente os Termos de Uso para se manter informado sobre quaisquer
                 mudanças.</p>
         </section>
 
         <section>
-            <h2>Privacidade</h2>
-            <p>O <strong>comandos.dev</strong> coleta e processa informações pessoais conforme descrito na nossa
+            <h2>privacidade</h2>
+            <p>o <strong>comandos.dev</strong> coleta e processa informações pessoais conforme descrito na nossa
                 <a href="/politica-de-privacidade"> Política de Privacidade</a>. Ao usar o site, você concorda com a coleta
                 e o uso dessas informações conforme estabelecido na política.</p>
         </section>
 
         <section>
-            <h2>Links de Terceiros</h2>
-            <p>O site pode conter links para sites de terceiros. O <strong>comandos.dev</strong> não se responsabiliza
+            <h2>links de Terceiros</h2>
+            <p>o site pode conter links para sites de terceiros. O <strong>comandos.dev</strong> não se responsabiliza
                 pelo conteúdo, práticas de privacidade ou segurança de sites externos. Ao acessar esses links, você o faz
                 por sua conta e risco.</p>
         </section>
 
         <section>
-            <h2>Limitação de Responsabilidade</h2>
-            <p>Na medida permitida pela lei aplicável, o <strong>comandos.dev</strong> não será responsável por quaisquer
+            <h2>limitação de Responsabilidade</h2>
+            <p>na medida permitida pela lei aplicável, o <strong>comandos.dev</strong> não será responsável por quaisquer
                 danos, perdas ou prejuízos resultantes do uso ou da incapacidade de uso da plataforma, incluindo danos
                 diretos, indiretos, incidentais, especiais ou consequenciais.</p>
         </section>
 
         <section>
-            <h2>Lei Aplicável</h2>
-            <p>Estes Termos de Uso serão regidos e interpretados de acordo com as leis do país onde o <strong>comandos.dev</strong> está registrado, sem considerar conflitos de princípios legais. Quaisquer disputas serão resolvidas nos tribunais competentes da jurisdição onde a empresa responsável pelo site está localizada.</p>
+            <h2>lei Aplicável</h2>
+            <p>estes Termos de Uso serão regidos e interpretados de acordo com as leis do país onde o <strong>comandos.dev</strong> está registrado, sem considerar conflitos de princípios legais. quaisquer disputas serão resolvidas nos tribunais competentes da jurisdição onde a empresa responsável pelo site está localizada.</p>
         </section>
 
         <section>
-            <h2>Contato</h2>
-            <p>Se você tiver dúvidas sobre estes Termos de Uso ou sobre o <strong>comandos.dev</strong>, entre em contato conosco através do seguinte endereço de e-mail: <a href="mailto:contato@comandos.dev">contato@comandos.dev</a>.</p>
+            <h2>contato</h2>
+            <p>se você tiver dúvidas sobre estes Termos de Uso ou sobre o <strong>comandos.dev</strong>, entre em contato conosco através do seguinte endereço de e-mail: <a href="mailto:contato@comandos.dev">comandos@gmail.com</a>.</p>
         </section>
 
     </article> 
