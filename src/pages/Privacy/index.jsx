91608646fb2c0b9b1074d425657f88e071cd390c
Leonardo Anjos <leoanjosmachadosilva@gmail.com>
2025-01-30 18:46:55 -0300

    corrections

--- src/pages/Privacy/index.jsx
+++ src/pages/Privacy/index.jsx
@@ -8,86 +8,86 @@ export const Privacy = () => {
             </header>
         
             <section>
-                <h2>Introdução</h2>
-                <p>A sua privacidade é muito importante para nós. Esta Política de Privacidade tem como objetivo informar
+                <h2>introdução</h2>
+                <p>a sua privacidade é muito importante para nós. Esta Política de Privacidade tem como objetivo informar
                     como o <strong>comandos.dev</strong> coleta, usa, armazena e protege as informações pessoais dos seus
                     usuários.</p>
-                <p>Ao utilizar o site <strong>comandos.dev</strong> ("site"), você concorda com a coleta e o uso das suas
+                <p>ao utilizar o site <strong>comandos.dev</strong> ("site"), você concorda com a coleta e o uso das suas
                     informações conforme descrito nesta Política de Privacidade.</p>
             </section>
 
             <section>
-                <h2>Informações Coletadas</h2>
-                <p>Coletamos diferentes tipos de informações com o objetivo de fornecer um serviço melhor e mais
+                <h2>informações Coletadas</h2>
+                <p>coletamos diferentes tipos de informações com o objetivo de fornecer um serviço melhor e mais
                     personalizado para os usuários. As informações podem ser coletadas de diferentes formas, como descrito
                     abaixo:</p>
 
-                <h3>Informações de Navegação</h3>
-                <p>Também podemos coletar automaticamente informações sobre a sua interação com o site, incluindo:</p>
+                <h3>informações de Navegação</h3>
+                <p>também podemos coletar automaticamente informações sobre a sua interação com o site, incluindo:</p>
                 <ul>
-                    <li>Endereço IP</li>
-                    <li>Tipo de navegador</li>
-                    <li>Sistema operacional</li>
-                    <li>Informações de cookies</li>
-                    <li>Dados de localização (se permitido)</li>
+                    <li>endereço IP</li>
+                    <li>tipo de navegador</li>
+                    <li>sistema operacional</li>
+                    <li>informações de cookies</li>
+                    <li>dados de localização (se permitido)</li>
                 </ul>
 
-                <h3>Cookies e Tecnologias Semelhantes</h3>
-                <p>Usamos <strong>cookies</strong> e tecnologias semelhantes para melhorar a experiência de navegação do
+                <h3>cookies e Tecnologias Semelhantes</h3>
+                <p>usamos <strong>cookies</strong> e tecnologias semelhantes para melhorar a experiência de navegação do
                     usuário, personalizar conteúdo, analisar o tráfego e realizar outras funções importantes para o
                     funcionamento do site.</p>
-                <p>Um <strong>cookie</strong> é um pequeno arquivo de texto armazenado no seu dispositivo que pode ser
+                <p>um <strong>cookie</strong> é um pequeno arquivo de texto armazenado no seu dispositivo que pode ser
                     utilizado para lembrar suas preferências ou fornecer outras funcionalidades.</p>
                 <p>Você pode desativar os cookies no seu navegador, mas isso pode afetar a funcionalidade do site.</p>
             </section>
 
             <section>
-                <h2>Uso das Informações</h2>
-                <p>As informações que coletamos são usadas para os seguintes fins:</p>
+                <h2>uso das Informações</h2>
+                <p>as informações que coletamos são usadas para os seguintes fins:</p>
                 <ul>
-                    <li><strong>Fornecer e Personalizar Serviços:</strong> Usamos suas informações para criar e personalizar sua
+                    <li><strong>fornecer e personalizar serviços:</strong> Usamos suas informações para criar e personalizar sua
                         experiência no <strong>comandos.dev</strong>, como salvar suas preferências e comandos favoritos.</li>
-                    <li><strong>Melhorar o Site:</strong> Para analisar o uso do site e melhorar seu desempenho e funcionalidades.</li>
-                    <li><strong>Comunicação:</strong> Podemos usar o seu e-mail para enviar notificações sobre atualizações, novos
+                    <li><strong>melhorar o Site:</strong> para analisar o uso do site e melhorar seu desempenho e funcionalidades.</li>
+                    <li><strong>comunicação:</strong> podemos usar o seu e-mail para enviar notificações sobre atualizações, novos
                         comandos ou outros conteúdos relevantes. Você pode optar por não receber essas comunicações a qualquer
                         momento.</li>
-                    <li><strong>Segurança e Cumprimento:</strong> Utilizamos suas informações para garantir a segurança do site e
+                    <li><strong>segurança e cumprimento:</strong> utilizamos suas informações para garantir a segurança do site e
                         para cumprir com as leis e regulamentos aplicáveis.</li>
                 </ul>
             </section>
 
             <section>
-                <h2>Compartilhamento de Informações</h2>
-                <p>Nós não vendemos, alugamos ou trocamos suas informações pessoais com terceiros. No entanto, podemos compartilhar
+                <h2>compartilhamento de informações</h2>
+                <p>nós não vendemos, alugamos ou trocamos suas informações pessoais com terceiros. No entanto, podemos compartilhar
                     suas informações nas seguintes circunstâncias:</p>
                 <ul>
-                    <li><strong>Com prestadores de serviços:</strong> Podemos compartilhar suas informações com fornecedores que
+                    <li><strong>com prestadores de serviços:</strong> podemos compartilhar suas informações com fornecedores que
                         ajudam a operar o site (como servidores de hospedagem, processadores de pagamentos, etc.), mas esses
                         fornecedores não têm permissão para usar suas informações para outros fins.</li>
-                    <li><strong>Em caso de exigência legal:</strong> Podemos divulgar suas informações pessoais se necessário para
+                    <li><strong>em caso de exigência legal:</strong> podemos divulgar suas informações pessoais se necessário para
                         cumprir com uma obrigação legal, proteger os direitos do site ou investigar atividades fraudulentas.</li>
                 </ul>
             </section>
 
             <section>
-                <h2>Armazenamento e Segurança dos Dados</h2>
-                <p>As informações pessoais que coletamos são armazenadas de maneira segura e protegidas por medidas de segurança
+                <h2>armazenamento e segurança dos dados</h2>
+                <p>as informações pessoais que coletamos são armazenadas de maneira segura e protegidas por medidas de segurança
                     adequadas. No entanto, nenhuma transmissão de dados pela internet ou sistema de armazenamento é completamente
                     segura. Apesar de nossos esforços, não podemos garantir a segurança total de suas informações.</p>
             </section>
 
             <section>
-                <h2>Seus Direitos</h2>
-                <p>De acordo com a legislação de privacidade vigente, você tem o direito de:</p>
+                <h2>seus Direitos</h2>
+                <p>de acordo com a legislação de privacidade vigente, você tem o direito de:</p>
                 <ul>
-                    <li><strong>Acessar:</strong> Solicitar uma cópia das suas informações pessoais que temos armazenadas.</li>
-                    <li><strong>Corrigir:</strong> Atualizar ou corrigir informações pessoais que estejam incorretas ou desatualizadas.</li>
-                    <li><strong>Excluir:</strong> Solicitar a exclusão de suas informações pessoais, desde que não haja uma razão legal
+                    <li><strong>acessar:</strong> solicitar uma cópia das suas informações pessoais que temos armazenadas.</li>
+                    <li><strong>corrigir:</strong> atualizar ou corrigir informações pessoais que estejam incorretas ou desatualizadas.</li>
+                    <li><strong>excluir:</strong> solicitar a exclusão de suas informações pessoais, desde que não haja uma razão legal
                         para mantê-las.</li>
-                    <li><strong>Opor-se ao processamento:</strong> Você pode, a qualquer momento, optar por não receber
+                    <li><strong>opor-se ao processamento:</strong> você pode, a qualquer momento, optar por não receber
                         comunicações ou retirar seu consentimento para o processamento de suas informações.</li>
                 </ul>
-                <p>Se desejar exercer algum desses direitos, entre em contato conosco através do e-mail <a href="mailto:contato@comandos.dev">contato@comandos.dev</a>.</p>
+                <p>se desejar exercer algum desses direitos, entre em contato conosco através do e-mail <a href="mailto:contato@comandos.dev">comandosdev@gmail.com</a>.</p>
             </section>
 
             <section>
