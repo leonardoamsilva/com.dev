c901abecd4167bd4907bf07ef77050ca4f6a0c98
Leonardo Anjos <leoanjosmachadosilva@gmail.com>
2025-02-02 12:46:44 -0300

    corrections

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -5,13 +5,13 @@ export const Contact = () => {
     <div className='container'>
     <header>
         <h1>contate-nos</h1>
-        <p>tem alguma dúvida ou sugestão? Estamos aqui para ajudar!</p>
+        <p>tem alguma dúvida ou sugestão? estamos aqui para ajudar!</p>
       </header>
 
       <section>
         <h2>outros canais de contato</h2>
         <p><strong>e-mail de suporte:</strong> <NavLink to="mailto:suporte@comandos.dev">comandosdev@gmail.com</NavLink></p>
-        <p><strong>redes Sociais:</strong></p>
+        <p><strong>redes sociais:</strong></p>
         <ul>
           <li>twitter: <NavLink to="https://twitter.com/comandosdev" target="_blank" rel="noopener noreferrer"> @comandosdev</NavLink></li>
           <li>gitHub: <NavLink to="https://github.com/comandos-dev" target="_blank" rel="noopener noreferrer">ComandosDev</NavLink></li>
