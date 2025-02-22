a5208a4a96366d391e535072c6c1196e3aec8eb8
Leonardo Anjos <leoanjosmachadosilva@gmail.com>
2025-02-22 14:39:25 -0300

    inclusion of ads

--- src/components/common/Info/index.jsx
+++ src/components/common/Info/index.jsx
@@ -5,6 +5,18 @@ export const Info = () => {
   return (
     <div className='info'>
       <p>ℹ️ a escolha por escrever tudo em minúsculas é proposital! 💡 ela faz parte da nossa identidade e é algo que muitos desenvolvedores gostam.  😉 <span><NavLink to="/feedback" >Curtiu? Conta pra gente! 💬</NavLink></span></p>
+      <div className="announcement">
+        <ins
+          class="adupeex"
+          data-ad-client="up-ba-332124"
+          data-ad-slot="37742"
+          data-ad-display="square"
+        >
+        </ins>
+        <script src="https://upeex.com.br/adsupeex.js" async>
+        </script>
+      </div>
+
     </div>
   );
 };
